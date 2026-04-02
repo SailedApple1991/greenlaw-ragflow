015fe56651cec6bae443f604552922fcd7b775cd
Yongteng Lei <yongtengrey@outlook.com>
2026-04-02 20:49:23 +0800

    Refa: Chat conversations /convsersation API to RESTFul (#13893)
    Chat conversations /convsersation API to RESTFul.
    
    - [x] Refactoring

--- web/src/pages/next-chats/chat/chat-box/multiple-chat-box.tsx
+++ web/src/pages/next-chats/chat/chat-box/multiple-chat-box.tsx
@@ -181,7 +181,7 @@ const ChatCard = forwardRef(function ChatCard(
                   avatarDialog={currentDialog.icon}
                   reference={buildMessageItemReference(
                     {
-                      message: derivedMessages,
+                      messages: derivedMessages,
                       reference: conversation.reference,
                     },
                     message,
--- web/src/pages/next-chats/hooks/use-send-chat-message.ts
+++ web/src/pages/next-chats/hooks/use-send-chat-message.ts
@@ -71,9 +71,8 @@ export const useSendMessage = (controller: AbortController) => {
   const { handleUploadFile, isUploading, removeFile, files, clearFiles } =
     useUploadFile();
 
-  const { send, answer, done } = useSendMessageWithSse(
-    api.completeConversation,
-  );
+  const { id: chatId } = useParams();
+  const { send, answer, done } = useSendMessageWithSse();
   const {
     scrollRef,
     messageContainerRef,
@@ -98,9 +97,10 @@ export const useSendMessage = (controller: AbortController) => {
       currentConversationId?: string;
       messages?: IMessage[];
     } & NextMessageInputOnPressEnterParameter) => {
+      const sessionId = currentConversationId ?? conversationId;
       const res = await send(
+        api.completionUrl(chatId!, sessionId),
         {
-          conversation_id: currentConversationId ?? conversationId,
           messages: [
             ...(Array.isArray(messages) && messages?.length > 0
               ? messages
@@ -123,6 +123,7 @@ export const useSendMessage = (controller: AbortController) => {
     [
       derivedMessages,
       conversationId,
+      chatId,
       removeLatestMessage,
       setValue,
       send,
